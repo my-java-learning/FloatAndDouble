736ff6a5e3e95322b40b91db838ea27ff4c427c6
Qsior <lukaszbojes@gmail.com>
2017-10-08 13:25:27 +0200

    Float and Double primitives with exercise

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,20 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //width of 32 (4 bytes)
+        int myIntValue = 5 / 3;
+        //width of 32 (4 bytes)
+        float myFloatValue = 5f / 3f;
+        //width of 64 (8 bytes)
+        double myDoubleValue = 5d / 3d;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
+
+        //exercise
+        double poundsNumber = 200d;
+        double kilogramsNumber = poundsNumber * 0.453_592_37d;
+        System.out.println("Kilograms : " + kilogramsNumber);
     }
 }
